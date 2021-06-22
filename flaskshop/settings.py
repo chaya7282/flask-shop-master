6ed0fa01487c9e15b5f1cb947595e9d7dfc91330
chayaZilb <chaya.zilberstein@gmail.com>
2021-06-22 21:20:54 +0300

    add procfile

--- flaskshop/settings.py
+++ flaskshop/settings.py
@@ -30,7 +30,7 @@ class Config:
     ]
 
     # SQLALCHEMY
-    SQLALCHEMY_DATABASE_URI =os.environ['REDISCLOUD_URL']
+    SQLALCHEMY_DATABASE_URI ="mysql://bcfdg4tjvjgh4k1m:i2ktthn65fssg9jf@qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qb21tsn0443f9qzn"
    # SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI", LocalConfig.db_uri)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
