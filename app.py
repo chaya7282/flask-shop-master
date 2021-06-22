4f153bfa09f157890d099e812c4547b2ee26fb8a
chayaZilb <chaya.zilberstein@gmail.com>
2021-06-22 16:08:47 +0300

    add procfile

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from flaskshop.app import create_app
-from flaskshop.app import commands
+from flaskshop import commands
+
 from flaskshop import app
 
 if __name__ == '__main__':
     app.run()
+
 
