72d7bba8697cdb0789aeacafdb18e28df1c956c7
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2021-07-29 10:29:21 +0300

    chane requ

--- flaskshop/settings.py
+++ flaskshop/settings.py
@@ -35,7 +35,7 @@ class Config:
 
     JSONIFY_PRETTYPRINT_REGULAR = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE_QUERY_TIMEOUT = 0.1  # log the slow database query, and unit is second
+    DATABASE_QUERY_TIMEOUT = 10  # log the slow database query, and unit is second
     SQLALCHEMY_RECORD_QUERIES = True
 
     SQLALCHEMY_POOL_SIZE= 100
