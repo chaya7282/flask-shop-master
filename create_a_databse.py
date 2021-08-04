4af8149f83295ded00abb6c1116bde4702d37b3e
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2021-08-04 14:04:38 +0300

    chane requ

--- create_a_databse.py
+++ create_a_databse.py
@@ -1,10 +1,7 @@
 import mysql.connector as mysql
-
+from flask_sqlalchemy import SQLAlchemy
 ## connecting to the database using 'connect()' method
 ## it takes 3 required parameters 'host', 'user', 'passwd'
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "dbms"
-)
+db =SQLAlchemy()
+
 print(db)
