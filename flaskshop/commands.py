13923204b68b51df165b509c2fafa433aec678a5
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2021-08-05 09:39:10 +0300

    chane requ

--- flaskshop/commands.py
+++ flaskshop/commands.py
@@ -230,7 +230,8 @@ def seed(type):
         for msg in create_generator:
             click.echo(msg)
     elif type == "product":
-
+        db.drop_all()
+        db.create_all()
         place_holder = Path("placeholders")
         create_products_by_schema(
             placeholder_dir=place_holder, how_many=1, create_images=False
