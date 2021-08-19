6ff2c8ab2aadca1aaca8abd65d8961dfb9df57dc
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2021-08-19 08:27:20 +0300

    chane requu

--- flaskshop/commands.py
+++ flaskshop/commands.py
@@ -204,6 +204,9 @@ def seed(type):
         create_generator = chain(
             create_roles(),
             create_admin(),
+            create_users(),
+            create_shipping_methods(),
+            create_orders(),
           create_dashboard_menus(),
         )
         for msg in create_generator:
