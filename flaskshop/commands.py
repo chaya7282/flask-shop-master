977a97f59abe362e7adb41f2270dadc355105f99
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2021-09-04 21:05:09 +0300

    Almost final- all functionality added

--- flaskshop/commands.py
+++ flaskshop/commands.py
@@ -234,15 +234,9 @@ def seed(type):
     elif type == "product":
 
         place_holder = Path("placeholders"),
-        create_generator = chain(
-
-            create_roles(),
-            create_admin(),
-
-            create_dashboard_menus(),
-        )
-
-
+        create_roles()
+        create_admin()
+        create_dashboard_menus()
 
         for msg in create_generator:
             click.echo(msg)
