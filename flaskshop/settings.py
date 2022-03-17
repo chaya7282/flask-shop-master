6342ef6294dc1515688c9ba38d9493c3554e2d04
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2022-03-17 21:44:59 +0200

    Almost final- all functionality added

--- flaskshop/settings.py
+++ flaskshop/settings.py
@@ -96,5 +96,5 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'chaya.zilberstein@gmail.com'
-    MAIL_PASSWORD = 'ChayaNadavYoav7282'
+    MAIL_USERNAME = 'esekmekuvan@gmail.com'
+    MAIL_PASSWORD = 'EsekMekuvan7282'
